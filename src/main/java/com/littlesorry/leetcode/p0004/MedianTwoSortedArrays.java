78881fbfff838342de7c7cac340f2d646a490a9c
wangbaofeng <wangbaofeng1778@yundasys.com>
2021-03-12 14:45:31 +0800

    ! correct p004

--- src/main/java/com/littlesorry/leetcode/p0004/MedianTwoSortedArrays.java
+++ src/main/java/com/littlesorry/leetcode/p0004/MedianTwoSortedArrays.java
@@ -18,8 +18,8 @@ public class MedianTwoSortedArrays {
         int[] sort = new int[mid];
         int i = 0, j = 0;
         for (int k = 0; k <= mid; k++) {
-            if (nums1[i] <= nums2[j]) sort[k] = nums1[i++];
-            else sort[k] = nums2[j++];
+            if (nums1[i] <= nums2[j]) sort[k] = nums1[min(i++, nums2.length -1)];
+            else sort[k] = nums2[min(j++, nums2.length -1)];
         }
 
         if ((total & 1) == 1) {
@@ -27,6 +27,34 @@ public class MedianTwoSortedArrays {
         } else {
             return ((double) sort[mid - 1] + sort[mid - 2]) / 2;
         }
+    }
+
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int i = 0, j = 0, step = 0, result = 0, current = 0
+                , centerLeft = (nums1.length + nums2.length - 1) / 2
+                , centerRight = (nums1.length + nums2.length) / 2;
+
+        for (;;) {
+            int val1 = i < nums1.length ? nums1[i] : Integer.MAX_VALUE;
+            int val2 = j < nums2.length ? nums2[j] : Integer.MAX_VALUE;
+
+            if (step == centerLeft) {
+                result += min(val1, val2);
+            }
+            if (step == centerRight) {
+                result += min(val1, val2);
+                break;
+            }
+
+            current = val1 < val2 ? i++ : j++;
+            step++;
+        }
+
+        return ((double) result) / 2;
+    }
+
+    private static int min(int a, int b) {
+        return a < b ? a : b;
     }
 
 }
---
+++ src/main/java/com/littlesorry/leetcode/p0076/MinWindow.java
@@ -0,0 +1,16 @@
+package com.littlesorry.leetcode.p0076;
+
+public class MinWindow {
+
+    public String minWindow(String s, String t) {
+//        int[] counterMap = new int[128];
+//
+//        for (char c : s.toCharArray()) {
+//            if (t.) {
+//
+//            }
+//        }
+
+        return "";
+    }
+}
