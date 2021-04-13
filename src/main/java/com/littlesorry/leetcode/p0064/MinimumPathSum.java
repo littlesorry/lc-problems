1d050706de6491c28bde8105e6643559e280b112
wangbaofeng <wangbaofeng1778@yundasys.com>
2021-04-13 09:35:21 +0800

    ! clean up

--- src/main/java/com/littlesorry/leetcode/p0064/MinimumPathSum.java
+++ src/main/java/com/littlesorry/leetcode/p0064/MinimumPathSum.java
@@ -3,8 +3,7 @@ package com.littlesorry.leetcode.p0064;
 public class MinimumPathSum {
 
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
 
         int[][] sums = new int[m][n];
         for (int i = 0; i < m; i++) {
@@ -12,15 +11,15 @@ public class MinimumPathSum {
                 if (i == 0 && j == 0) {
                     sums[i][j] = grid[i][j];
                 } else if (i == 0) {
-                    sums[i][j] = sums[i][j-1] + grid[i][j];
+                    sums[i][j] = sums[i][j - 1] + grid[i][j];
                 } else if (j == 0) {
-                    sums[i][j] = sums[i-1][j] + grid[i][j];
+                    sums[i][j] = sums[i - 1][j] + grid[i][j];
                 } else {
                     sums[i][j] = (Math.min(sums[i - 1][j], sums[i][j - 1])) + grid[i][j];
                 }
             }
         }
 
-        return sums[m-1][n-1];
+        return sums[m - 1][n - 1];
     }
 }
