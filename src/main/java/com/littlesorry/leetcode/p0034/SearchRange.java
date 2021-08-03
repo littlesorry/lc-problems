cc3989c0d9592733c8a2f0fc00ba6717e7c751e3
wangbaofeng <wangbaofeng1778@yundasys.com>
2021-08-03 11:24:55 +0800

    ! update p34

--- src/main/java/com/littlesorry/leetcode/p0034/SearchRange.java
+++ src/main/java/com/littlesorry/leetcode/p0034/SearchRange.java
@@ -2,36 +2,50 @@ package com.littlesorry.leetcode.p0034;
 
 public class SearchRange {
 
-    public static void main(String[] args) {
-        System.out.println(new SearchRange().searchLeft(new int[]{5,7,7,8,8,10}, 8, 0, 5));
-    }
-
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{searchLeft(nums, target, 0, nums.length - 1)
-                , searchRight(nums, target, 0, nums.length - 1)};
+        int idx = search(nums, target, 0, nums.length - 1);
+        return range(nums, idx);
     }
 
-    public int searchLeft(int[] nums, int target, int left, int right) {
-        if (left >= right) {
-            return nums[left] == target ? left : -1;
+    public int search(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
         }
 
-        if (nums[(right + left) / 2] > target) {
-            return searchLeft(nums, target, left, (right + left) / 2);
+        int mid = (start + end) / 2;
+        if (nums[mid] > target) {
+            return search(nums, target, start, mid - 1);
+        } else if (nums[mid] < target) {
+            return search(nums, target, mid + 1, end);
+        } else {
+            // equals
+            return mid;
         }
-
-        return searchLeft(nums, target, (right + left) / 2 + 1, right);
     }
 
-    public int searchRight(int[] nums, int target, int left, int right) {
-        if (left >= right) {
-            return nums[left] == target ? left : -1;
+    public int[] range(int[] nums, int idx) {
+
+        if (idx == -1) {
+            return new int[]{-1, -1};
         }
 
-        if (nums[(right + left) / 2] < target) {
-            return searchRight(nums, target, (right + left) / 2 + 1, right);
+        int[] result = new int[]{idx, idx};
+        for (int i = idx - 1; i >= 0; i--) {
+            if (nums[i] == nums[idx]) {
+                result[0] = i;
+            } else {
+                break;
+            }
         }
 
-        return searchRight(nums, target, left, (right + left) / 2);
+        for (int i = idx + 1; i < nums.length; i++) {
+            if (nums[i] == nums[idx]) {
+                result[1] = i;
+            } else {
+                break;
+            }
+        }
+
+        return result;
     }
 }
